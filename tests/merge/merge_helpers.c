d02f6b9cbe4817895789c0fd6507336ce7cc98fc
ranbingbing <ranbingbing@springup.xyz>
2020-12-03 14:27:28 +0800

    run test github actions 27

--- tests/merge/merge_helpers.c
+++ tests/merge/merge_helpers.c
@@ -303,6 +303,9 @@ int merge_test_conflicts(git_conflict *conflicts,const struct merge_conflict_pat
 			case 1:
 				printf("[case 1]");
 				printf("[%s]",conflicts->diffs[j].ancestor_entry.path);
+				if (strcmp(expected[i].path,"expected[i].path")!=0){
+					printf("strcmp>>>");
+				}
 				// if (strcmp(expected[i].path,conflicts->diffs[j].ancestor_entry.path)==0){
 					flag=0;
 				// }
